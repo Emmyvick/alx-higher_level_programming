b00d414b2f98d852bf9d7370bd2e4c01b950b73f
root <Emmyvick>
2022-10-25 16:31:20 -0700

    Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,20 +1,23 @@
-#!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
+!/usr/bin/python3
+'''A module for managing students.
+'''
 
-from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
+class Student:
+    '''Represents a student.
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''Initializes this student with the given first name,
+        last name, and age.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+    def to_json(self):
+        '''Retrieves a dictionary of this student's attributes.
+        Returns:
+            dict: A dictionary of this student's attributes.
+        '''
+        if '__dict__' in dir(self):
+            return self.__dict__
